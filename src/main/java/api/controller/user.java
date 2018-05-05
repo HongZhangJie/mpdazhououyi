f15a2a199183ec8924e86e6a786354ee19201825
HongZhangJie <770414975@qq.com>
2018-05-05 18:33:41 +0800

    1.0
    小程序后台1.0
    未初始化

---
+++ src/main/java/api/controller/user.java
@@ -0,0 +1,20 @@
+package api.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+/**
+ * 数据交互层
+ * @author 
+ *
+ */
